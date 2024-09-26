46f0323ff915c566cea91e4e99f29ae223979709
Ariel <ariequeve@yahoo.com.ar>
2024-09-26 18:55:26 -0300

    agregro script para ejercicio goleadores

---
+++ tabla-goleadores.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+// Definir una estructura para almacenar la información de un jugador "struct Jugador"
+
+
+/* Definir una Funciónn "cargarDatosjugador" para la carga de los datos
+Parametros: debe recibir un array de jugadores y el número de Jugadores a procesar
+Funcionalidad: La función carga en las estructuras de datos la información ingresada por el usuario
+Return: nada
+*/
+
+
+/* Definir una Función "ordenarGoleador" para ordenar los Jugador por mejor tiempo (Alg de ordenamiento burbuja)
+Parámetros: debe recibir un array de piolotos y la cantidad de Jugadores
+Funcionliadad: realiza un ordenamiento teniendo en cuenta los tiempos de cada pioloto de menor a mayor
+Return: nada
+*/
+
+
+/* Definir una Función "MostarEquipoGoleador()" que calcula y devuelve al equipo más goleador
+Parámetros: debe recibir el array de jugadores
+Funcionalidad: Identifica el mejor equipo en función de la cantidad de goles de sus jugadores
+Return: el nombre del equipo y la cantidad de goles totales
+*/ 
+
+
+/* Definir una Funcion muestroTablaGoleadores() para mostrar en pantalla la Tabla de Goleadores
+Parámetros: recibe un array de jugadores y el número de Jugadores
+Funcionalidad: Muestra en terminar la tabla de golaadores 
+Return: nada
+*/ 
+
+
+//Aplicación que calcula la Grilla de partida en funcion de los tiempos de clasificacion de los piolotos
+int main() {
+
+    // Cargo los datos de los Jugadores con la funcion cargarDatosJugador() teniendo en cuenta sus parámetros y el valor de return
+    
+    // Ordeno los Jugadores por el mejor tiempo con la funcion ordenarJugador() teniendo en cuenta sus parámetros y el valor de return
+
+    // Identifico al equipo con mayor cantidad de goles con la función MostarEquipoGoleador() teniendo en cuenta sus parámetros y el valor de return
+
+    // Mostrar la tabla de goleadores y al equipo más goleador con la fumción muestroGrilla() teniendo en cuenta sus parámetros y el valor de return
+
+
+    return 0;
+}
